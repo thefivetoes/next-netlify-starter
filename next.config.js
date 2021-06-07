82cbde2066b31c9af7fb0ea0a58aa4a8018c92af
justin smith <justin@jws.io>
2021-06-07 14:44:40 -0400

    Log some messages during build

--- next.config.js
+++ next.config.js
@@ -1,16 +1,19 @@
 module.exports = {
   webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
-      if (isServer) {
-          const glob = require("glob");
-          const allArticles = glob.sync("./_articles/*.json");
-          const fs = require("fs");
-          const articleContent = allArticles.map(article => {
-              var content = fs.readFileSync(article, "utf8");
-              return JSON.parse(content);
-          });
-          global.allArticles = articleContent;
-          // Get a list of all Articles
-      }
+    console.log("Doing webpacking")
+    if (isServer) {
+      console.log("is server")
+      const glob = require("glob");
+      const allArticles = glob.sync("./_articles/*.json");
+      const fs = require("fs");
+      const articleContent = allArticles.map((article) => {
+        var content = fs.readFileSync(article, "utf8");
+        console.log(content);
+        return JSON.parse(content);
+      });
+      global.allArticles = articleContent;
+      // Get a list of all Articles
+    }
     // Important: return the modified config
     return config;
   },
